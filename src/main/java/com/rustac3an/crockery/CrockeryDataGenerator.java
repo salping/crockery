c1160349ef32dd27ede22fc63fb40926c6c9b61c
indie <93843215+indideee@users.noreply.github.com>
2024-07-02 21:52:02 +1200

    first (second) commit

---
+++ src/main/java/com/rustac3an/crockery/CrockeryDataGenerator.java
@@ -0,0 +1,11 @@
+package com.rustac3an.crockery;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CrockeryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
